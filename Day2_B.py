761abc441d7ee2ae723503e7196fa757f67179e0
CarlBliwxt <carl.pingis@gmail.com>
2021-12-20 10:55:19 +0100

    -update

---
+++ Day2_B.py
@@ -0,0 +1,21 @@
+with open('input02.txt', 'r') as f: 
+    words = f.read().split()
+
+i = 1
+n = 0
+horizontal = 0
+depth = 0
+aim = 0 
+while i <= len(words):
+    if str(words[n]) == "forward" :
+        horizontal += int(words[i])
+        depth += aim * int(words[i])
+    if str(words[n]) == "down" :
+        aim += int(words[i])
+    if str(words[n]) == "up" :
+       aim -= int(words[i])
+    i += 1
+    n += 1
+
+print('part 1:', horizontal * depth)
+
